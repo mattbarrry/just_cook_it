67155a1ff4628d251ce4601eefc81aee545f7e2c
Matt Barry <mbarry@gmail.com>
2023-02-27 20:24:15 -0500

    fix top nav login url

--- spec/helpers/navigation_helper_spec.rb
+++ spec/helpers/navigation_helper_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe NavigationHelper, type: :helper do
     context 'when there is no logged in user' do
       it 'shows login and signup buttons' do
         expect(subject).to have_tag(:a, text: 'Sign up')
-        expect(subject).to have_tag(:a, text: 'Log in', with: { href: login_path })
+        expect(subject).to have_tag(:a, text: 'Log in', with: { href: new_login_path })
       end
     end
   end
